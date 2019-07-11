ec338212ad1acd9b3a50a055dc28e32acee5676a
Vitor Rios <vr.vitorrios@gmail.com>
2019-07-11 07:43:04 -0300

    Fazendo upload para o firebase storage

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -1,11 +1,19 @@
-const multer = require('multer');
+const Multer = require('multer');
 const path = require('path');
 
 module.exports = {
-    storage: new multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'uploads'),
-        filename: function(req, file, cb){
+    // storage: new multer.diskStorage({
+    //     destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    //     filename: function(req, file, cb){
+    //         cb(null, file.originalname);
+    //     }
+    // })
+    storage: Multer.memoryStorage({
+        filename: function (req, file, cb) {
             cb(null, file.originalname);
         }
-    })
+    }),
+    limits: {
+        fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
+    }
 }
--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -3,6 +3,8 @@ const sharp = require('sharp');
 const path = require('path');
 const fs = require('fs');
 
+const { storage } = require('../config/firebase')
+
 module.exports = {
 
     async index(req, res) {
@@ -13,19 +15,29 @@ module.exports = {
 
     async store(req, res) {
         const { author, place, description, hashtags } = req.body;
-        const { filename: image } = req.file;
-
+        const { originalname: image } = req.file;
+console.log(req.file)
         const [name] = image.split('.');
 
-        const fileName = `${name}.jpg`;
+        const fileName = `${name}.png`;
+   
+        let file = req.file;
 
+        if (file) {
+            uploadImageToFirebase(file).then((success) => {
+                console.log(success)
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+/*
         await sharp(req.file.path)
             .resize(500)
             .jpeg({ quality: 70 })
             .toFile(path.resolve(req.file.destination, 'resized', fileName))
 
         fs.unlinkSync(req.file.path)
-
+*/
         const post = await Post.create({
             author,
             place,
@@ -39,4 +51,43 @@ module.exports = {
         return res.json(post);
     }
 };
+
+
+
+const uploadImageToFirebase = (file) => {
+    return new Promise((resolve, reject) => {
+
+        if (!file) reject('No image file');
+        console.log(file)
+        let newFileName = `${Date.now()}_${file.originalname}`
+        console.log(newFileName)
+
+        const bucket = storage().bucket('cloneapp-instagram.appspot.com')
+
+        let fileUpload = bucket.file(newFileName);
+        //storage().bucket('cloneapp-instagram.appspot.com').upload(file.path)
+
+        console.log(fileUpload.name)
+
+        const blobStream = fileUpload.createWriteStream({
+            metadata: {
+                contentType: file.mimetype
+            }
+        });
+
+        blobStream.on('error', (error) => {
+            reject('Something is wrong! Unable to upload at the moment.' + error);
+        })
+
+        blobStream.on('finish', () => {
+            // The public URL can be used to directly access the file via HTTP.
+            
+            const url = `https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`
+            console.log(url)
+            resolve(url);
+        });
+
+        blobStream.end(file.buffer);
+    })
+}
 
